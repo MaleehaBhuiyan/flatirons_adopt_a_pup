bba87da2792e9893c35d8b56ff94e81ed27a69bb
MaleehaBhuiyan <maleehabhuiyan@baruchmail.cuny.edu>
2020-07-08 17:42:16 -0400

    a lot of stuff

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,11 +1,34 @@
 class PetsController < ApplicationController
 
     def index 
-        @pets = Pet.all  
+        @pets = Pet.all.select do |pet|
+            pet.user_id == nil 
+        end 
     end 
 
     def show
         @pet = Pet.find(params[:id])
+    end 
+
+    def edit
+        @pet = Pet.find(params[:id])
+        @users = User.all 
+    end
+    
+    def update 
+        @pet= Pet.find(params[:id]) 
+        if @pet.update(pet_params)
+            redirect_to pet_path(@pet.id)
+          else 
+            flash[:my_errors] = @pet.errors.full_messages
+            redirect_to edit_pet_path
+        end 
+    end 
+
+    private 
+
+    def pet_params
+        params.require(:pet).permit!
     end 
 
 end
